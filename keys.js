0d5dfae6f44515219db2857eef2bf17e1d430a9d
DeeFG <xdeishax@gmail.com>
2019-05-31 02:42:26 -0400

    bands

--- keys.js
+++ keys.js
@@ -1,24 +1,47 @@
 
+
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.476ec4513b934cf4b593fcac79981767,
-  secret: process.env.c4adb3e56883466f8526073f678ddd7e 
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
-var Spotify = require('node-spotify-api');
+
+// var SpotifyWebApi = require('spotify-web-api-node');
+
+// // credentials are optional
+// var spotifyApi = new SpotifyWebApi({
+//   clientId: '',
+//   clientSecret: '',
+//   redirectUri: 'http://www.example.com/callback'
+// });
+
+// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+//   function(data) {
+//     console.log('Artist albums', data.body);
+//   },
+//   function(err) {
+//     console.error(err);
+//   }
+// );
+
+
+
+
  
-var spotify = new Spotify({
-  id: 476ec4513b934cf4b593fcac79981767 , 
-  secret: c4adb3e56883466f8526073f678ddd7e
-});
+// spotify.search(
+//   { type: 'track', 
+//   query: 'All the Small Things' 
+// }, 
+  
+//   function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+
+
+// });
 
 
--- liri.js
+++ liri.js
@@ -1,25 +1,52 @@
-require("dotenv").config();
-var keys = require("./keys.js");
+const dotenv = require("dotenv").config();
+const axios = require('axios');
+const keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// const bandsAPI = "ea62aafa-9c71-4187-9044-fe3057cbd186"
 
-keys.prompt([
-    {
-      type: "input",
-      message: " What band do you like?",
-      name: ""
-    }
-  ])
-  .then(function(Response) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-    if (Response) {
-      console.log(Response);
-   
-    }
-    else {
-      console.log(Response);
-    }
-  });
+let searched ="zed";
+//const bandsURL = ("https://rest.bandsintown.com/" + searched + "ea62aafa-9c71-4187-9044-fe3057cbd186");
+
+let URL = "https://rest.bandsintown.com/artists/"+ searched +"/events?app_id=ea62aafa-9c71-4187-9044-fe3057cbd186"
+
+
+
+// Bands in town
+axios.get(URL).then(
+
+  function (response) {
+console.log( "*******************************************************************************************************");
+console.log(response.data.id);
+console.log( response);
+    // console.log("The Venue" + response.VenueData.name);
+    // console.log("The Location" + response.VenueData.city + "," + response.VenueData.region + "," + response.VenueData.country);
+  // console.log("The Date " + window.JSON.EventData.datetime);
+    // (use moment to format this as "MM/DD/YYYY")
+
+  }
+);
+
+//
+
+
+
+
+
+
+
+// let URL = + api
+// axios.get('/user?ID=12345')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
 
 
 
